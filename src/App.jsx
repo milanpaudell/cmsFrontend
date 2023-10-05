b1b724c0b232ccf9998b477047dda301ab2a559b
milanpaudell <milanpaudell31@gmail.com>
2023-10-05 22:33:41 +0545

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-      <h1>Milan Paudel</h1>
+      <h1 style={{color : 'grey',}}>Milan Paudel</h1>
       <h3 style={myCssObject}>I am a student</h3>
       <h5>Age : {13+14}</h5>
       <h4>{name}</h4>
