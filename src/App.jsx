6f225fef82f43ab103c1ffaf68d40dd3890195af
milanpaudell <milanpaudell31@gmail.com>
2023-10-05 22:17:54 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  const name = 'Milan'
+
+  const myCssObject = {
+    color: 'red',
+  }
+
+  return (
+    <>
+      <h1>Milan Paudel</h1>
+      <h3 style={myCssObject}>I am a student</h3>
+      <h5>Age : {13+14}</h5>
+      <h4>{name}</h4>
+    </>
+  )
+}
+
+export default App
